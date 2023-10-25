e38cfa540c60f35693844cadf9b401b571d3354f
Noel Gutierrez-Brizuela <noelgb@taroko.ldeo.columbia.edu>
2023-10-25 13:34:41 -0400

    finalized time_fixer routine

--- data_access.py
+++ data_access.py
@@ -1,9 +1,10 @@
-# data_acces.py
+# data_access.py
 '''
 Module featuring routines that enable easy access to files stored in Taroko, Berimbau, and Kage. 
 '''
 import xarray as xr; import pandas as pd; 
 from datetime import datetime, timedelta; 
+import numbers
 
 # Route to notable SST products.
 urls = dict(); 
@@ -14,25 +15,37 @@ urls['COBE2']= 'http://kage.ldeo.columbia.edu:81/home/.datasets/.COBE-SST2/.sst.
 urls['ERSSTv5'] = 'http://kage.ldeo.columbia.edu:81/home/.datasets/.ERSST/.ERSSTv5.nc/.sst/zlev/removeGRID/dods'
 start = {'HadISST':1870,'ORAs5':1958,'COBE':1891,'COBE2':1850,'ERSSTv5':1854}
 
+dt = {'HadISST':timedelta(days = 1), 'ORAs5':timedelta(days=1), 
+      'ERSSTv5':timedelta(days= 365.25/12 ),  'COBE':timedelta(days=1), 
+      'COBE2': timedelta(days=1) }
+
+def hour_rounder(t):
+    # Rounds to nearest hour by adding a timedelta hour if minute >= 30
+    return (t.replace(second=0, microsecond=0, minute=0, hour=t.hour)
+               +timedelta(hours=t.minute//30))
+
 class dataset:
     # Class for gridded datasets. 
     def __init__( self, name ):
         self.name = name;
         self.url = urls[name];
         self.start = datetime( start[name], 1, 1, 0 ); # hour zero of Jan 1 of given years
+        self.dt = dt[name]
+        self.ds = self.ds_loader()
         
+    def ds_loader( self ):
+        temp_ds = xr.load_dataset( self.url, decode_times = False );
+        temp_ds = self.time_fixer( temp_ds );
+        return temp_ds
     
-    @property
-    def ds(self):
-        return self._ds
-
-    @ds.setter
-    def ds( self, opts = None ):
-        temp_ds = xr.load_dataset( self.name , decode_times = False );
-        self._ds = time_maker( temp_ds );
-
-
-
-
-
-
+    def time_fixer( self , xr ):
+        try:
+            if isinstance( xr['time'].values[0] , numbers.Number):
+                xr['time'] = [ hour_rounder( \
+                        self.start + self.dt * jj ) for jj in xr['time'].values ]
+            else: 
+                print( 'Time coordinates are not numeric')
+        except:
+            print('Coordinate time does not exist.')
+        return xr
+        
