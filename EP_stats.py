0a09204e2839a8edebdd268b92910be4bf5bfc21
Noel Gutierrez-Brizuela <noelgb@crhtc46.hpc.ucar.edu>
2023-11-14 12:36:58 -0700

    redefined t0 in seasonal_cycle to ensure coef0 = mean

--- EP_stats.py
+++ EP_stats.py
@@ -11,7 +11,9 @@ def seasonal_matrix( timevec, n_terms , leap_years = False ):
     if leap_years:
         year = 356.25
 
-    N = len( timevec ); t0 = datetime( 1900, 1, 1 );
+    N = len( timevec ); 
+    # t0 must be middle point in order for coef0 to be the mean
+    t0 = pd.to_datetime( timevec.isel( time = int( len(timevec)/2 ) ).values );
     xaxis = np.array([ (pd.to_datetime(jj) - t0 ).total_seconds() \
             for jj in timevec.values ])
     year_freq = 2 * np.pi / (3600*24*year)
