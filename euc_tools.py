48fdd3b93f47eb2f6ed334ad4a39d823cd875ab9
Noel Gutierrez-Brizuela <noelgb@ldeo.columbia.edu>
2024-02-23 11:18:55 -0500

    added function to compute terms in KE budget

--- euc_tools.py
+++ euc_tools.py
@@ -16,11 +16,10 @@ def get_euc_area( ds ):
 def find_euc( ds ):
     # Take ocean dataset from pop2 and apply conditions to 
     # return a mask that is True in locations that are the EUC
-    
     # Begin with spatial subsetting
     ds = get_euc_area( ds ); 
     # Now physical / ocean-dependent conditions
-    is_eastward = ds['UVEL'] > 5; # in cm / s 
+    is_eastward = ds['UVEL'] > 10; # in cm / s 
     below_ML = ds['z_t'] > ( ds['HMXL']/100 );
     # Combine masks
     euc_mask = is_eastward * below_ML
@@ -36,10 +35,26 @@ def get_euc_transport( ds, sum_over = ['lat','z_t'] ):
     return transport 
 
 def find_euc_bottom( ds ):
+    # FOR SIMPLICITY I THINK THIS WILL HAVE TO BECOME FIND EUC_CORE
     # Start by calculating EUC transport without summing over z_t
-    transport = get_euc_transport( ds , sum_over = ['lat'] ); 
-    vert_int = transport.sum(['z_t']);
-    # Now use cumsum to find depths above which 95% of transport is
+    transport = get_euc_transport( ds , sum_over = ['lat'] );
+    # Already multiplied by dy and dz. 
+    vert_int = ( transport * dz ).sum(['z_t']); # total transport across all depth
+    # Now find depths above which 95% of transport is
     cum_trans = transport.cumsum( ['z_t'] ); 
-    pass
+        
+def KE_budget( ds ):
+    # Evaluate terms in the kinetic energy budget of the EUC
+    ds = get_euc_area( ds )
+    # Basic ingredients
+    u = ( ds['UVEL']/100 ).persist()
+    dudz = - u.differentiate('z_t').persist() ;
+    rho = 1024; # kg m^-3
+    # Calculate viscous and vertical-advective components of KE change
+    vert = ( u * ds['WVEL']/100 * dudz * rho ).persist()
+    visc = ( - rho * u * ( ds['VVC'] * 1e-4  * dudz ).differentiate('z_t') ).persist()
+    return vert, visc 
+    
+
+
 
